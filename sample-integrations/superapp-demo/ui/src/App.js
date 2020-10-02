1e5b43c754a73e61847a352b9631b666f322c2ae
xiko <Kobuta23>
2020-10-02 16:00:53 +0100

    first push

--- sample-integrations/superapp-demo/ui/src/App.js
+++ sample-integrations/superapp-demo/ui/src/App.js
@@ -3,20 +3,26 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Web3Provider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
 
-import { Body, Button, Header, Image } from "./components";
+import {
+  Body,
+  Button,
+  Header,
+  BoxContainer,
+  Box,
+  Left,
+  Center
+} from "./components";
 import { web3Modal, logoutOfWeb3Modal } from "./utils/web3Modal";
 import logo from "./logo.jpeg";
 
 import GET_TRANSFERS from "./graphql/subgraph";
 const TruffleContract = require("@truffle/contract");
 
-const APP_ADDRESS = "0xAA8fCf1DE73d609346360Ea7e84e618B4d464a8E";
+const APP_ADDRESS = "0x3bF9DCDD87c31855C901f13AF20B0956140971EF";
 const MINIMUM_GAME_FLOW_RATE = "3858024691358";
 const LotterySuperApp = TruffleContract(require("./LotterySuperApp.json"));
 
-const {
-    wad4human,
-} = require("@decentral.ee/web3-helpers");
+const { wad4human } = require("@decentral.ee/web3-helpers");
 
 const SuperfluidSDK = require("@superfluid-finance/ethereum-contracts");
 
@@ -38,19 +44,24 @@ function WalletButton({ provider, loadWeb3Modal }) {
 
 function TableOfPlayers({ playerList, winner }) {
   var items = [];
-  for (const [index, value] of [playerList].entries()) {
-    const { address, netFlow } = value;
-    items.push(
-      <li key={index}>
-        {address} {address === winner ? "👑" : ""} ~~~ {netFlow}
-      </li>
-    );
+  console.log(playerList);
+  if (playerList.length > 1) {
+    for (const [index, value] of [playerList].entries()) {
+      const { address, netFlow } = value;
+      console.log("address: ", address, " netFlow: ", netFlow);
+      items.push(
+        <li key={index}>
+          {address} {address === winner ? "👑" : ""} ~~~{" "}
+          {(netFlow * (3600 * 24 * 30)) / 1e18}
+        </li>
+      );
+    }
   }
-
   return <ul>{items}</ul>;
 }
 
 function showTick(bool) {
+  if (typeof bool === "undefined") return "";
   if (bool) return "✔️";
   else return "❌";
 }
@@ -61,11 +72,10 @@ let daix;
 let app;
 
 function App() {
-
   const { loading, error, data } = useQuery(GET_TRANSFERS);
   const [provider, setProvider] = useState();
-  const [daiApproved, setDAIapproved] = useState(false);
-  const [joinedLottery, setJoinedLottery] = useState(false);
+  const [daiApproved, setDAIapproved] = useState();
+  const [joinedLottery, setJoinedLottery] = useState();
   const [userAddress, setUserAddress] = useState("");
   const [winnerAddress, setWinnerAddress] = useState("NOBODY");
   const [daiBalance, setDaiBalance] = useState(0);
@@ -73,16 +83,13 @@ function App() {
   const [userNetFlow, setUserNetFlow] = useState(0);
   const [playerList, setPlayerList] = useState([]);
 
-  function addPlayer({ player }) {
-    setPlayerList([...playerList, player]);
-  }
-
   async function mintDAI(amount = 100) {
     //mint some dai here!  100 default amount
     await dai.mint(
       userAddress,
       sf.web3.utils.toWei(amount.toString(), "ether"),
-      { from: userAddress });
+      { from: userAddress }
+    );
     setDaiBalance(wad4human(await dai.balanceOf.call(userAddress)));
   }
 
@@ -91,57 +98,59 @@ function App() {
     await dai.approve(
       daix.address,
       "115792089237316195423570985008687907853269984665640564039457584007913129639935",
-      { from: userAddress});
+      { from: userAddress }
+    );
     setDAIapproved(true);
   }
 
   async function joinLottery() {
     console.log("app", app);
     await sf.host.batchCall(
+      [
         [
-            [
-                2, // upgrade 100 daix to play the game
-                daix.address,
-                sf.web3.eth.abi.encodeParameters(
-                    ["uint256"],
-                    [sf.web3.utils.toWei("100", "ether").toString()])
-            ],
-            [
-                0, // approve the ticket fee
-                daix.address,
-                sf.web3.eth.abi.encodeParameters(
-                    ["address", "uint256"],
-                    [APP_ADDRESS, sf.web3.utils.toWei("1", "ether").toString()])
-            ],
-            [
-                5, // callAppAction to participate
-                app.address,
-                app.contract.methods.participate("0x").encodeABI()
-            ],
-            [
-                4, // create constant flow (10/mo)
-                sf.agreements.cfa.address,
-                sf.agreements.cfa.contract.methods.createFlow(
-                    daix.address,
-                    app.address,
-                    MINIMUM_GAME_FLOW_RATE.toString(),
-                    "0x"
-                ).encodeABI()
-            ]
+          2, // upgrade 100 daix to play the game
+          daix.address,
+          sf.web3.eth.abi.encodeParameters(
+            ["uint256"],
+            [sf.web3.utils.toWei("100", "ether").toString()]
+          )
         ],
-        { from: userAddress }
+        [
+          0, // approve the ticket fee
+          daix.address,
+          sf.web3.eth.abi.encodeParameters(
+            ["address", "uint256"],
+            [APP_ADDRESS, sf.web3.utils.toWei("1", "ether").toString()]
+          )
+        ],
+        [
+          5, // callAppAction to participate
+          app.address,
+          app.contract.methods.participate("0x").encodeABI()
+        ],
+        [
+          4, // create constant flow (10/mo)
+          sf.agreements.cfa.address,
+          sf.agreements.cfa.contract.methods
+            .createFlow(
+              daix.address,
+              app.address,
+              MINIMUM_GAME_FLOW_RATE.toString(),
+              "0x"
+            )
+            .encodeABI()
+        ]
+      ],
+      { from: userAddress }
     );
   }
 
   async function leaveLottery() {
     await sf.host.callAgreement(
       sf.agreements.cfa.address,
-      sf.agreements.cfa.contract.methods.deleteFlow(
-          daix.address,
-          userAddress,
-          app.address,
-          "0x"
-      ).encodeABI(),
+      sf.agreements.cfa.contract.methods
+        .deleteFlow(daix.address, userAddress, app.address, "0x")
+        .encodeABI(),
       { from: userAddress }
     );
   }
@@ -151,9 +160,9 @@ function App() {
     const newProvider = await web3Modal.connect();
 
     sf = new SuperfluidSDK.Framework({
-        chainId: 5,
-        version: "master",
-        web3Provider: newProvider,
+      chainId: 5,
+      version: "master",
+      web3Provider: newProvider
     });
     await sf.initialize();
 
@@ -175,26 +184,60 @@ function App() {
     if (web3Modal.cachedProvider) {
       loadWeb3Modal();
     }
+
     // ############################ here you do all the data retrieval: please pull all the current players in the lottery and push them using addPlayer({address, netFlow})
   }, [loadWeb3Modal]);
 
+  function getLatestFlows(flows) {
+    return Object.values(
+      flows.reduce((acc, i) => {
+        acc[i.args.sender + ":" + i.args.receiver] = i;
+        return acc;
+      }, {})
+    ).filter(i => i.args.flowRate.toString() != "0");
+  }
   useEffect(() => {
     (async () => {
       if (provider) {
         setDaiBalance(wad4human(await dai.balanceOf.call(userAddress)));
         setDaixBalance(wad4human(await daix.balanceOf.call(userAddress)));
-        if ((await dai.allowance.call(daix.address, userAddress)).toString() !== "0") {
+        if (
+          (await dai.allowance.call(daix.address, userAddress)).toString() !==
+          "0"
+        ) {
           setDAIapproved(true);
-        };
+        }
         setUserNetFlow(
-          (await sf.agreements.cfa.getNetFlow.call(daix.address, userAddress)).toString()
-          * 3600 * 24 * 30
-          / 1e18
+          ((await sf.agreements.cfa.getNetFlow.call(
+            daix.address,
+            userAddress
+          )).toString() *
+            3600 *
+            24 *
+            30) /
+            1e18
         );
         setJoinedLottery(
-          (await sf.agreements.cfa.getFlow(daix.address, userAddress, app.address)).timestamp
-          > 0);
+          (await sf.agreements.cfa.getFlow(
+            daix.address,
+            userAddress,
+            app.address
+          )).timestamp > 0
+        );
         setWinnerAddress((await app.currentWinner.call()).player);
+        getLatestFlows(
+          await sf.agreements.cfa.getPastEvents("FlowUpdated", {
+            fromBlock: 0,
+            filter: {
+              receiver: app.address
+            }
+          })
+        ).map(f =>
+          setPlayerList({
+            address: f.args.sender,
+            flowRate: f.args.flowRate.toString() * ((3600 * 24 * 30) / 1e18)
+          })
+        );
       }
     })();
   }, [provider, userAddress, winnerAddress]);
@@ -209,32 +252,54 @@ function App() {
     <div>
       <Header>
         <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} />
+        <Left>A simple lottery app, built on Superfluid streams!</Left>
       </Header>
-      <Body>
-        <Image src={logo} alt="react-logo" />
-        <p>A simple lottery app, built on Superfluid streams!</p>
+      <Body
+        winner={
+          !joinedLottery
+            ? "notPlaying"
+            : winnerAddress === userAddress
+            ? "winner"
+            : "loser"
+        }
+      >
+        {/*<Image src={logo} alt="react-logo" />*/}
         {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button onClick={() => mintDAI()}>
-          Mint some DAI {showTick(daiBalance !== "0")}
-        </Button>
-        <Button onClick={() => approveDAI()}>
-          Approve DAI {showTick(daiApproved)}
-        </Button>
-        <Button onClick={() => joinLottery()}>
-          Join Lottery {showTick(!joinedLottery)}
-        </Button>
-        <Button onClick={() => leaveLottery()}>
-          leaveLottery Lottery {showTick(joinedLottery)}
-        </Button>
-        <div>
-          <p> Your DAI balance: {daiBalance}</p>
-          <p> Your DAIx balance: {daixBalance}</p>
-          <p> Your netFlow: {userNetFlow}</p>
-          <p> Current Winner: {winnerAddress}</p>
-        </div>
-        <div>
-          <TableOfPlayers playerList={playerList} winner={winnerAddress} />
-        </div>
+        <BoxContainer>
+          <Box>
+            <div>
+              <p> Your DAI balance: {daiBalance}</p>
+              <p> Your DAIx balance: {daixBalance}</p>
+              <p> Your netFlow: {userNetFlow}</p>
+              {joinedLottery && winnerAddress === userAddress && (
+                <p>YOU're winning!</p>
+              )}
+            </div>
+          </Box>
+          <Box>
+            <Center>
+              {daiBalance === "0" ? (
+                <Button onClick={() => mintDAI()}>
+                  Mint some DAI {showTick(daiBalance !== "0")}
+                </Button>
+              ) : (
+                <Button onClick={() => approveDAI()}>
+                  Approve DAI {showTick(daiApproved)}
+                </Button>
+              )}
+              {joinedLottery === false ? (
+                <Button onClick={() => joinLottery()}>Join Lottery</Button>
+              ) : (
+                <Button onClick={() => leaveLottery()}>Leave Lottery</Button>
+              )}
+            </Center>
+          </Box>
+          <Box>
+            <div>
+              <TableOfPlayers playerList={playerList} winner={winnerAddress} />
+            </div>
+          </Box>
+        </BoxContainer>
       </Body>
     </div>
   );
--- sample-integrations/superapp-demo/ui/src/components/index.js
+++ sample-integrations/superapp-demo/ui/src/components/index.js
@@ -1,24 +1,36 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-  background-color: #282c34;
+  background-color: #000;
   min-height: 70px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: flex-start;
   color: white;
 `;
 
 export const Body = styled.body`
   align-items: center;
-  background-color: #282c34;
   color: white;
-  display: flex;
-  flex-direction: column;
-  font-size: calc(10px + 2vmin);
+  font-size: 15px;
   justify-content: center;
   min-height: calc(100vh - 70px);
+  background-color: #000;
+  background-size: 100vh; /* You must set a specified height */
+  background-position-x: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  font-family: "Rock Salt", cursive;
+  ${({ winner }) => {
+    switch (winner) {
+      case "winner":
+        return `background-image: url("./winning.gif");`;
+      case "loser":
+        return `background-image: url("./waterfall.gif");`;
+      default:
+        return `background-color: #000;`;
+    }
+  }}
 `;
 
 export const Image = styled.img`
@@ -36,19 +48,41 @@ export const Link = styled.a.attrs({
 `;
 
 export const Button = styled.button`
-  background-color: white;
+  background-color: #cd4337;
   border: none;
   border-radius: 8px;
-  color: #282c34;
+  color: #fff;
+  box-shadow: 0px 0px 15px 8px rgba(200, 200, 255, 0.75);
   cursor: pointer;
   font-size: 16px;
   text-align: center;
   text-decoration: none;
-  margin: 0px 20px;
+  margin: 10px 20px;
   padding: 12px 24px;
 
   ${props => props.hidden && "hidden"} :focus {
     border: none;
     outline: none;
   }
+`;
+
+export const Center = styled.div`
+  text-align: center;
+`;
+
+export const Box = styled.div`
+  font-size: 1em;
+  flex-grow: 1;
+  max-width: 33%;
+  padding: 14px;
+`;
+
+export const BoxContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`;
+
+export const Left = styled.span`
+  text-align: left !important;
 `;
